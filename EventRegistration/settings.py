effd3714e9a17d54d39a2c92a92488ec8bbba63c
sagorluc <mdsagorluc@gmail.com>
2024-02-26 20:54:49 +0600

    update

--- EventRegistration/settings.py
+++ EventRegistration/settings.py
@@ -18,8 +18,9 @@ FILTERS_DISABLE_HELP_TEXT = True
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
